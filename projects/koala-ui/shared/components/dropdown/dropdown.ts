0f75c64c6c3c9134d22f77dce09af2e796bfd8b7
Igor Rangel <igordrangel@gmail.com>
2025-07-17 23:35:40 -0300

    feat: implementar lógica de posicionamento dinâmico no componente Dropdown

--- projects/koala-ui/shared/components/dropdown/dropdown.ts
+++ projects/koala-ui/shared/components/dropdown/dropdown.ts
@@ -1,7 +1,62 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  ElementRef,
+  input,
+  viewChild,
+} from '@angular/core';
+
+type DropdownVerticalPosition = 'top' | 'bottom' | 'left' | 'right';
+type DropdownHorizontalPosition = 'start' | 'center' | 'end';
 
 @Component({
   selector: 'kl-dropdown',
   templateUrl: './dropdown.html',
 })
-export class Dropdown {}
+export class Dropdown {
+  private readonly dropdownElement =
+    viewChild<ElementRef<HTMLDivElement>>('dropdown');
+
+  verticalPosition = input<DropdownVerticalPosition>('bottom');
+  horizontalPosition = input<DropdownHorizontalPosition>('start');
+
+  verticalPositionClass = computed(() => {
+    switch (this.verticalPosition()) {
+      case 'top':
+        return 'dropdown-top';
+      case 'left':
+        return 'dropdown-left';
+      case 'right':
+        return 'dropdown-right';
+      case 'bottom':
+      default:
+        return 'dropdown-bottom';
+    }
+  });
+
+  horizontalPositionClass = computed(() => {
+    switch (this.horizontalPosition()) {
+      case 'start':
+        return 'dropdown-start';
+      case 'center':
+        return 'dropdown-center';
+      case 'end':
+      default:
+        return 'dropdown-end';
+    }
+  });
+
+  constructor() {
+    effect(() => {
+      const dropdownElement = this.dropdownElement()?.nativeElement;
+
+      if (dropdownElement) {
+        dropdownElement.classList.add(
+          this.verticalPositionClass(),
+          this.horizontalPositionClass()
+        );
+      }
+    });
+  }
+}
