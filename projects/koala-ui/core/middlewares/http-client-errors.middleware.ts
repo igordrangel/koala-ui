ce04596b3d99877fb56d4f136ec465def87d62b5
Igor Rangel <igor.rangel_meta@prestador.globo>
2026-03-31 16:32:55 -0300

    feat: adicionar lógica para ignorar erros no middleware de tratamento de erros HTTP

--- projects/koala-ui/core/middlewares/http-client-errors.middleware.ts
+++ projects/koala-ui/core/middlewares/http-client-errors.middleware.ts
@@ -2,4 +2,5 @@ import { HttpErrorResponse } from '@angular/common/http';
 
 export abstract class HttpClientErrorsMiddleware {
   abstract handleError(response: HttpErrorResponse): string;
+  abstract ignoreError(response: HttpErrorResponse): boolean;
 }
